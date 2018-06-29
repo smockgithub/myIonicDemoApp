ce872515f0152311d3bde7bdeb709a06b746033a
董煜 <smock@homepc.com>
2018-06-30 00:57:58 +0800

    0

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,62 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  window = <any>window;
+  isAndroid:boolean;
 
+  constructor(public navCtrl: NavController,platform: Platform) {
+    platform.ready().then(() => {
+      this.isAndroid = platform.is('android');
+    });
   }
+
+  isInstall(){
+    if (typeof (<any>window).Wechat === "undefined") {
+      console.log("Wechat plugin is not installed.");
+      return false;
+    }
+  }
+
+
+  startJD(){
+    if (typeof this.window.cordova === "undefined") {
+      console.log("cordova plugin is not installed.");
+      return false;
+    }
+    var arg0 = "";
+    debugger;
+    this.window.cordova.plugins.CordovaPluginSmock1.coolMethod(arg0, this.success, this.error)
+  }
+
+  success(msg) {
+    debugger;
+    alert('success:'+msg);
+  }
+  error(msg) {
+    debugger;
+    alert('error:'+msg);
+  }
+
+
+  updateApp(){
+    if(this.isAndroid){
+      debugger;
+      this.window.DownInstall.autoDInstallAPK(["newapp",    //your app fileName
+                    "http://reg.wabaoge.com/assets/test.apk",    //your apk download url
+                    "app download",   //your apk download description
+                    "new app downloading"],   //your apk download title
+                    function(ok) {
+                      debugger;
+                    },
+                    function(error) {
+                      debugger;
+                       if("permission refused"==error){
+                          alert("permission refused");
+                        }
+                    });
+
+    }
+  }
+
 
 }
